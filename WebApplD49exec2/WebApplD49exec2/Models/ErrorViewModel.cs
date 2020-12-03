1da017dabee3a98aed8af77bf64c607847adad1e
rahuldangwal100 <rahul.sd200@gmail.com>
2020-12-03 16:02:43 +0530

    first commit

---
+++ WebApplD49exec2/WebApplD49exec2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplD49exec2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
