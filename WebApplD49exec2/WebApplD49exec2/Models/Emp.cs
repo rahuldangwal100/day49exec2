2170200f057360605f5d76d1bb2fe4c62a2ab4a1
rahuldangwal100 <rahul.sd200@gmail.com>
2020-12-03 16:13:42 +0530

    second commit

---
+++ WebApplD49exec2/WebApplD49exec2/Models/Emp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplD49exec2.Models
+{
+    public class Emp
+    {
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesig { get; set; }
+
+        public string EDoj { get; set; }
+    }
+}
